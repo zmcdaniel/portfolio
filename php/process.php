4aa8ba8966af0c4a2c4858ce558c6e3a2c5a60aa
zmcdaniel <zmcdaniel@gmail.com>
2016-11-14 13:22:20 -0800

    PHP script to recieve and process data

--- php/process.php
+++ php/process.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zaoies
- * Date: 11/14/16
- * Time: 1:00 PM
- */
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+$EmailTo = "zmcdaniel@gmail.com";
+$Subject = "New Message Received from zoemcdaniel.com";
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success){
+    echo "success";
+}else{
+    echo "invalid";
+}
+
+?>
